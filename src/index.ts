d6840965aebabe5532936429e1a30547a82884f3
ChiragFullstack <chiragmahajan9019@gmail.com>
2025-09-20 20:27:55 +0530

    Allow Cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import connectDB from "./config/db";
-
+import cors from "cors";
 import userRoutes from "./routes/userRoutes";
 import couponRoutes from "./routes/couponRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
