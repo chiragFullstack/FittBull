03b266f137f8a64327e4d7599f2bef51cf810daf
ChiragFullstack <chiragmahajan9019@gmail.com>
2025-09-20 20:36:21 +0530

    update cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,11 @@ import couponRoutes from "./routes/couponRoutes";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 
